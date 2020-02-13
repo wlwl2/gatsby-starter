79bb169bd08db63548a4f9d4cc5ada869a9d06a8
William Lim <manvinedi@gmail.com>
2020-02-13 16:34:15 +0800

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,26 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import css from "./header.module.css"
+
+const Header = ({ siteTitle }) => (
+  <header className={css.header}>
+    <div className={css.headerInnerContainer}>
+      <h1 className={css.h1}>
+        <Link to="/" className={css.titleLink}>
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/utils/typography.js
@@ -0,0 +1,4 @@
+import Typography from "typography";
+import moragaTheme from 'typography-theme-moraga'
+const typography = new Typography(moragaTheme)
+export default typography
